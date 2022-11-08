0e5a8e7109f9a1430597496f2d1a248187e3e41b
jake-ephraim <ephraimakolo2017@gmail.com>
2022-11-08 16:55:33 +0100

    created same_faces template

--- app.py
+++ app.py
@@ -5,11 +5,13 @@ from pydantic import BaseModel
 app = FastAPI()
 
 USERPATH = "./user"
+USERSMILINGPATH = "./usersimiling"
 IDPATH = "./id"
 
 
 class VerifyData(BaseModel):
     user_image: str
+    user_image_smiling: str
     id_image: str
     file_type: str
 
@@ -18,6 +20,9 @@ def save_file(file_bin:str|list, file_path:str|list, file_type:str):
     pass
 
 def its_a_face(image:str, file_type:str) -> bool:
+    pass
+
+def same_faces(images:str|list, file_type:str) -> bool:
     pass
 
 @app.get('/')
@@ -35,16 +40,25 @@ def make_verification(request_data:VerifyData):
     authorize_user = False
     msg = ""
     user_image = request_data.user_image
+    user_image_smiling = request_data.user_image_smiling
     id_image = request_data.id_image
     file_type = request_data.file_type
     try:
-        save_file([user_image, id_image], [USERPATH, IDPATH], file_type)
+        save_file([user_image, user_image_smiling, id_image], [USERPATH, USERSMILINGPATH, IDPATH], file_type)
         if not its_a_face(USERPATH, file_type):
             msg = "User image not a face"
             raise(Exception("User image not a face"))
+        if not its_a_face(USERSMILINGPATH, file_type):
+            msg = "User smiling image not a face"
+            raise(Exception("User smiling image not a face"))
         if not its_a_face(IDPATH, file_type):
             msg = "ID image not a face"
             raise(Exception("ID image not a face"))
+        if n:=same_faces([USERPATH, USERSMILINGPATH, IDPATH], file_type):
+            authorize_user = True
+            msg = "Faces match"
+        else:
+            msg = "User & user smiling image mismatch" if n == 1 else "User & ID image mismatch"
     except Exception as e:
         pass
     return {"Authorize User": authorize_user, "msg": msg}
